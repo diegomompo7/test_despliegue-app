10547772b6ecc970fd5f143c82c41f6514c4f952
Diego Mompó Redoli <diego.mompo.redoli@gmail.com>
2023-03-22 21:41:57 +0100

    act

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola amigos!</h1>
+
+      <HashRouter>
+        <div className="links">
+          <Link to="/">HomePage</Link>
+          <br />
+          <Link to="/notes">NotesPage</Link>
+          <br />
+          <Link to="/toggles">TogglablePage</Link>
+          <br />
+        </div>
+
+        <Routes>
+          <Route path="/notes" element={<HomePage></HomePage>}></Route>
+          <Route path="/toggles" element={<Togglable></Togglable>}></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
